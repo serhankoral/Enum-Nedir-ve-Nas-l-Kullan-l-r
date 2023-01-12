c3f618962c65e066b31c719ed270c96512296393
Serhan KORAL <serhan_koral@hotmail.com>
2023-01-12 16:10:15 +0300

    Proje dosyası ekle.

---
+++ Enum Nedir ve Nasıl Kullanılır/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Enum_Nedir_ve_Nasıl_Kullanılır
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
