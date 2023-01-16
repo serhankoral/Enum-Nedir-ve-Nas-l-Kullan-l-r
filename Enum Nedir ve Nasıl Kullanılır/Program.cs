7a11ab5f1ecb44786b4ffcb2bd305a451a7fe06c
Serhan KORAL <serhan_koral@hotmail.com>
2023-01-16 12:48:53 +0300

    Bitti.

--- Enum Nedir ve Nasıl Kullanılır/Program.cs
+++ Enum Nedir ve Nasıl Kullanılır/Program.cs
@@ -15,8 +15,29 @@ namespace Enum_Nedir_ve_Nasıl_Kullanılır
              * Enum sabit değerleri alır sonradan değiştirilemez
              * enum tanımlamaları calss dışında yapılır  
              */
-            Console.WriteLine("Haftanın 1.Günü {0}",Sabitler.tarihBilgileri.gunler.Pazartesi);  
-            Sabitler.tarihBilgileri.aylar.Şubat
+            Console.WriteLine("Haftanın 1.Günü {0}",Sabitler.tarihBilgileri.gunler.Pazartesi);
+            Console.WriteLine(Sabitler.tarihBilgileri.aylar.Şubat);
+            Console.WriteLine( "{0} Ayı yılım {1}. ayıdır.", Sabitler.tarihBilgileri.aylar.Şubat,((int)Sabitler.tarihBilgileri.aylar.Şubat));
+            Console.WriteLine("Enumun type ile kullanımı {0}",Enum.Parse(typeof(Sabitler.tarihBilgileri.aylar),"5"));
+            Console.WriteLine("Enumun içerisinde int olan değere karşılık gelen değeri Getname ile alınır");
+            Console.WriteLine(Enum.GetName(typeof(Sabitler.tarihBilgileri.gunler), 5));
+            Console.WriteLine("Tüm Değerleri String dizi ile alırı");
+            string[] icerik = Enum.GetNames(typeof(Sabitler.tarihBilgileri.aylar));
+            Console.WriteLine("Aylar Enum içeriğinin string karşılığı");
+            foreach (var item in icerik)
+            {
+                Console.Write("{0}",item);
+
+            }
+            Console.WriteLine("Aylar Enum içeriğinin string ve int karşılığı");
+            
+            foreach (var item in Enum.GetValues(typeof(Sabitler.tarihBilgileri.aylar)))
+            {
+                Console.Write("{0}=",item);
+                Console.WriteLine((int)item);
+            }
+            
+
         }
 
     }
